e799738f6d057c11f5db0f704b9cfb7ac396d25e
Lucas ferry <lucas.ferry.ecole@gmail.com>
2025-01-09 21:38:33 +0100

    delete french comment

--- backend/api/middleware/authMiddleware.js
+++ backend/api/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ module.exports = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   if (!authHeader) return res.status(401).json({ error: 'Token missing' });
 
-  // Récupérer le token après "Bearer "
   const token = authHeader.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'Token format invalid' });
 
@@ -16,7 +15,7 @@ module.exports = (req, res, next) => {
       }
       return res.status(403).json({ error: 'Invalid token' });
     }
-    req.user = decoded; // Stocke les infos décodées dans `req.user`
+    req.user = decoded;
     next();
   });
 };
