0b92ac500ad5c3e354f8affa8cd8bb99cbfe933a
uendi_muca <umuca21@epoka.edu.al>
2025-01-10 18:08:23 +0100

    updates

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
+
+
+app.use(express.json());  // Built-in JSON body parser
+
+
+// Mount authentication routes
+app.use('/api/auth', authRoutes);  // This will handle /api/auth/login
+app.use('/api/users', userRoutes); 
 
 module.exports = app;
