64bc8bbada1eaefe79d1e35d6b527ff58a67cdad
LucasFerry <Lucas.ferry.ecole@gmail.com>
2025-01-18 18:51:38 +0100

    Add cors config to back

--- backend/api/middleware/corsMiddleware.js
+++ backend/api/middleware/corsMiddleware.js
@@ -1,8 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
- 
+  origin: ['http://localhost:5173', 'https://intelli-secure.tom-fourcaudot.com'],
 };
 
 module.exports = cors(corsOptions);
