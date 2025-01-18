e70db982702da28b38a9fea0cffb1b3b8b499c7a
LucasFerry <Lucas.ferry.ecole@gmail.com>
2025-01-18 18:58:25 +0100

    Enable all cors

--- backend/api/middleware/corsMiddleware.js
+++ backend/api/middleware/corsMiddleware.js
@@ -1,7 +1,3 @@
 const cors = require('cors');
 
-const corsOptions = {
-  origin: ['http://localhost:5173', 'https://intelli-secure.tom-fourcaudot.com'],
-};
-
-module.exports = cors(corsOptions);
+module.exports = cors();
