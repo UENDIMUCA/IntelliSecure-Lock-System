29d83fd80e02b60014d0d37dc553149fc7ee4311
LucasFerry <Lucas.ferry.ecole@gmail.com>
2025-01-20 19:53:31 +0100

    show temp user

--- frontend/src/components/AccountTable.tsx
+++ frontend/src/components/AccountTable.tsx
@@ -1,6 +1,6 @@
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow,} from "@/components/ui/table"
 import {Button} from "@/components/ui/button.tsx";
-import {Trash, User as UserSVG} from "lucide-react";
+import {ShieldBan, ShieldCheck, Trash, User as UserSVG} from "lucide-react";
 import {User} from "@/lib/types.ts";
 import {useToast} from "@/hooks/use-toast.ts";
 
@@ -21,6 +21,7 @@ export default function AccountTable({users, connectedUser}: AccountProp ) {
           <TableHead>E-mail</TableHead>
           <TableHead>Date de création</TableHead>
           <TableHead>Role</TableHead>
+          <TableHead>Active</TableHead>
           <TableHead>Action</TableHead>
         </TableRow>
       </TableHeader>
@@ -30,12 +31,30 @@ export default function AccountTable({users, connectedUser}: AccountProp ) {
             const isNotMe = user.id !== connectedUser.id;
             const isAdmin = user.isAdmin ? 'Admin' : 'User';
             const date = new Date(user.createdAt);
+            const isTemp = user.beginDate && user.endDate;
+            let isActive = true;
+            if (isTemp) {
+              isActive = new Date() >= new Date(new Date(user.beginDate).setHours(0, 0, 0, 0)) && new Date() <= new Date(new Date(user.endDate).setHours(23, 59, 59, 999));
+            }
             return (
               <TableRow key={user.id}>
                 <TableCell>{user.username}</TableCell>
                 <TableCell>{user.email}</TableCell>
                 <TableCell>{date.toLocaleString()}</TableCell>
                 <TableCell>{isAdmin}</TableCell>
+                <TableCell>
+                  <div className='flex flex-row items-center gap-1'>
+                    {
+                      isActive
+                        ? <ShieldCheck />
+                        : <ShieldBan />
+                    }
+                    {user.beginDate && user.endDate
+                    ? `Temporary user from ${new Date(user.beginDate).toLocaleDateString()} to ${new Date(user.endDate).toLocaleDateString()}`
+                    : "Permanent user"
+                    }
+                  </div>
+                </TableCell>
                 <TableCell className={"flex flex-col gap-1 items-center md:flex-row"}>
                   {isNotMe
                     ? <Button variant={"destructive"} size={"icon"}
